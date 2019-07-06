4dccc0d7dcb4fbbc6b398c8e22d0b12fd0a1ad69
ThomazStaziak <thostaziak@gmail.com>
2019-07-06 03:10:31 -0300

    adding the migration to the database

---
+++ database/migrations/2019_07_05_205823_create_leads_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLeadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leads', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->nullable(false);
+            $table->string('email')->unique();
+            // for a while
+            $table->string('state')->nullable();
+            // for a while
+            $table->string('city')->nullable();
+            $table->bigInteger('phone_number')->nullable();
+            $table->string('contact_time')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leads');
+    }
+}
